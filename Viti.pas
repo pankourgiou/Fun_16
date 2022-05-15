239530ef130094e25ac36afd75f7f038d6dfd49b
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-05-15 12:40:04 +0300

    Update and rename Viti.pas to drei.py

--- Viti.pas
+++
@@ -1,8 +0,0 @@
-#from urllib.request import urlopen
-#page=urlopen("http://vitisport.de/")
-
-#Fetches the code
-#of the webpage
-#content = page.read()
-
-#print(content)
---
+++ drei.py
@@ -0,0 +1,12 @@
+import time
+import antigravity
+
+start = time.time()
+print("=+-=三")
+
+xyz = "0+-=三"
+
+print(bool(xyz))
+
+end = time.time()
+print(end - start)
