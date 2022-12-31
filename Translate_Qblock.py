db714902e5ea281fb3f64abe1a4b4faa2a10c511
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-12-31 03:23:44 +0200

    Update Translate_Qblock.py

--- Translate_Qblock.py
+++ Translate_Qblock.py
@@ -1,4 +1,31 @@
 #Install translators module
+#the alphabet sounds from a-z are:
+#ba
+#bo
+#bi
+#da
+#de
+#fa
+#go
+#ha
+#hi
+#jo
+#ka
+#lo
+#ma
+#na
+#no
+#pa
+#qa
+#ra
+#sa
+#ta
+#ua
+#va
+#wa
+#xa
+#ya
+#za
 import translators as ts
 
 text = 'cheese'
